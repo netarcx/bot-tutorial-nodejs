bf896daa39b421c9b9afd6d11d887811064d6143
netarcx <trentfox1@me.com>
2016-08-01 21:05:46 -0500

    1816

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var blue = require('thebluealliance')
 var initTBA = require('thebluealliance');
 var tba = initTBA('node-thebluealliance','Node.js wrapper library for the TBA v2 API','1.1.1');
 
-console.log(tba.getTeamsAtEvent());
+console.log(tba.getTeamsAtEvent(1816));
 
 
 var botID = process.env.BOT_ID;
